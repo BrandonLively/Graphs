031aae20f7b1cf2803c02ded949ffbc432ba8e38
BrandonLively <akutenshi@live.com>
2020-03-18 17:42:40 -0400

    Refactored ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,13 +1,13 @@
 def earliest_ancestor(ancestors, starting_node):
-    children = [starting_node]
+    children = get_parents(ancestors, [starting_node])
     result = -1
     while True:
-        children = get_parents(ancestors, children)
-        if len(get_parents(ancestors, children)) == 0:
+        next_children = get_parents(ancestors, children)
+        if len(next_children) == 0:
             if len(children) > 0:
                 result = children[0]
             return result
-
+        children = next_children
 
 
 def get_parents(ancestors, children):
@@ -17,3 +17,4 @@ def get_parents(ancestors, children):
             if parent[1] == child:
                 parents.append(parent[0])
     return parents
+
